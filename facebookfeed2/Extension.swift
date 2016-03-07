4018f44cee2bd8bfc98f2854119654e0d4b16cb4
Zizheng Wu <me@zizhengwu.com>
2016-03-06 20:03:10 -0500

    split classes into different files

---
+++ facebookfeed2/Extension.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+}
+
+extension UIView {
+    
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerate() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+    
+}
---
+++ facebookfeed2/Post.swift
@@ -0,0 +1,8 @@
+class Post {
+    var name: String?
+    var profileImageName: String?
+    var statusText: String?
+    var statusImageName: String?
+    var numLikes: Int?
+    var numComments: Int?
+}
