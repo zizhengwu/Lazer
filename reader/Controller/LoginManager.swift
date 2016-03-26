c77ecf88fc17c7bb14961bfdc457f0ee8f13ef27
Zizheng Wu <me@zizhengwu.com>
2016-03-26 16:55:27 -0400

    AWSCognito could sign in with Facebook

--- reader/Controller/LoginManager.swift
+++ reader/Controller/LoginManager.swift
@@ -6,12 +6,44 @@ import FBSDKShareKit
 import FBSDKLoginKit
 
 class LoginManager {
+    
     static let sharedInstance = LoginManager()
+    
+    var cognitoId: String
+    var loggedIn: Bool
+    var userImage: UIImage?
+    var userName: String?
+    var userEmail: String?
     
     init() {
         let credentialsProvider = AWSCognitoCredentialsProvider(regionType: Constant.COGNITO_REGIONTYPE, identityPoolId: Constant.COGNITO_IDENTITY_POOL_ID)
-        if (FBSDKAccessToken.currentAccessToken() != nil) {
+        let configuration = AWSServiceConfiguration(region: .USEast1, credentialsProvider:credentialsProvider)
+        AWSServiceManager.defaultServiceManager().defaultServiceConfiguration = configuration
+        
+        if let cachedIdentityId = credentialsProvider.identityId {
+            print("cognito logged in")
+            loggedIn = true
+            cognitoId = cachedIdentityId
+        }
+        else {
+            print("cognitoId not logged in")
+            loggedIn = false
+            cognitoId = ""
+        }
+        
+        if let token = FBSDKAccessToken.currentAccessToken().tokenString {
+            credentialsProvider.logins = [AWSCognitoLoginProviderKey.Facebook.rawValue: token]
             print("Facebook already logged in")
+            credentialsProvider.getIdentityId().continueWithBlock { (task: AWSTask!) -> AnyObject! in
+                if (task.error != nil) {
+                    print("Error: " + task.error!.localizedDescription)
+                } else {
+                    // the task result will contain the identity id
+                    let _ = task.result
+                    print(credentialsProvider.getIdentityId())
+                }
+                return nil
+            }
         }
         else {
             print("Facebook not logged in")
