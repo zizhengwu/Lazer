1648b3ba88dbe5bd82c3677219747628a9ec50bf
Zizheng Wu <me@zizhengwu.com>
2016-03-26 18:57:30 -0400

    fetch profile

--- reader/Controller/LoginManager.swift
+++ reader/Controller/LoginManager.swift
@@ -9,27 +9,18 @@ class LoginManager {
     
     static let sharedInstance = LoginManager()
     
-    var cognitoId: String
-    var loggedIn: Bool
+    var cognitoId: String?
+    var loggedIn: Bool?
     var userImage: UIImage?
     var userName: String?
     var userEmail: String?
+    var syncClient: AWSCognito?
+    var dataset: AWSCognitoDataset?
     
     init() {
         let credentialsProvider = AWSCognitoCredentialsProvider(regionType: Constant.COGNITO_REGIONTYPE, identityPoolId: Constant.COGNITO_IDENTITY_POOL_ID)
         let configuration = AWSServiceConfiguration(region: .USEast1, credentialsProvider:credentialsProvider)
         AWSServiceManager.defaultServiceManager().defaultServiceConfiguration = configuration
-        
-        if let cachedIdentityId = credentialsProvider.identityId {
-            print("cognito logged in")
-            loggedIn = true
-            cognitoId = cachedIdentityId
-        }
-        else {
-            print("cognitoId not logged in")
-            loggedIn = false
-            cognitoId = ""
-        }
         
         if let token = FBSDKAccessToken.currentAccessToken() {
             credentialsProvider.logins = [AWSCognitoLoginProviderKey.Facebook.rawValue: token.tokenString]
@@ -41,6 +32,9 @@ class LoginManager {
                     // the task result will contain the identity id
                     let _ = task.result
                     print(credentialsProvider.getIdentityId())
+                    self.loggedIn = true
+                    self.sync()
+                    self.fetchProfile()
                 }
                 return nil
             }
@@ -48,5 +42,57 @@ class LoginManager {
         else {
             print("Facebook not logged in")
         }
+    }
+    
+    func sync() {
+        self.syncClient = AWSCognito.defaultCognito()
+        self.dataset = self.syncClient!.openOrCreateDataset("preference")
+        
+        self.dataset!.synchronize().continueWithBlock {(task) -> AnyObject! in
+            
+            if task.error != nil {
+                // Error while executing task
+                
+            } else {
+                // Task succeeded. The data was saved in the sync store.
+                
+            }
+            return nil
+        }
+    }
+    
+    func fetchProfile() {
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).startWithCompletionHandler({ (connection, user, requestError) -> Void in
+            
+            if requestError != nil {
+                print(requestError)
+                return
+            }
+            
+            var _ = user["email"] as String!
+            let firstName = user["first_name"] as String!
+            let lastName = user["last_name"] as String!
+            
+            self.userName = "\(firstName!) \(lastName!)"
+            
+            let userDict = user as? NSDictionary?
+            let pictureUrl = userDict!!["picture"]!["data"]!["url"]! as String!
+            
+            let url = NSURL(string: pictureUrl)
+            NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                if error != nil {
+                    print(error)
+                    return
+                }
+                
+                let image = UIImage(data: data!)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.userImage = image
+                })
+                
+            }).resume()
+            
+        })
     }
 }
