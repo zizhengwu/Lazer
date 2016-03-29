3bccbcccbb5a737dea92259187f2c52527c697a2
Zizheng Wu <me@zizhengwu.com>
2016-03-29 03:22:23 -0400

    refine the Facebook login page

--- reader/Controller/LoginViewController.swift
+++ reader/Controller/LoginViewController.swift
@@ -4,21 +4,21 @@ import FBSDKLoginKit
 
 class LoginViewController: UIViewController, FBSDKLoginButtonDelegate  {
     
-    let userImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .ScaleAspectFit
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.layer.masksToBounds = true
-        return imageView
-    }()
+//    let userImageView: UIImageView = {
+//        let imageView = UIImageView()
+//        imageView.contentMode = .ScaleAspectFit
+//        imageView.translatesAutoresizingMaskIntoConstraints = false
+//        imageView.layer.masksToBounds = true
+//        return imageView
+//    }()
     
-    let nameLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.boldSystemFontOfSize(20)
-        label.textAlignment = .Center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+//    let nameLabel: UILabel = {
+//        let label = UILabel()
+//        label.font = UIFont.boldSystemFontOfSize(20)
+//        label.textAlignment = .Center
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        return label
+//    }()
     
     let loginButton: FBSDKLoginButton = {
         let button = FBSDKLoginButton()
@@ -40,15 +40,19 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate  {
     
     func setupSubviews() {
         view.addSubview(loginButton)
-        view.addSubview(userImageView)
-        view.addSubview(nameLabel)
+//        view.addSubview(userImageView)
+//        view.addSubview(nameLabel)
         
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": userImageView]))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+//        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": userImageView]))
+//        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
         
         view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-80-[v0]-80-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": loginButton]))
         
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-8-[v0(100)]-8-[v1(30)]-8-[v2(50)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": userImageView, "v1": nameLabel, "v2": loginButton]))
+        loginButton.snp_makeConstraints{ (make) -> Void in
+            make.centerY.equalTo(view).offset(80)
+        }
+        
+//        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-8-[v0(100)]-8-[v1(30)]-8-[v2(50)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": userImageView, "v1": nameLabel, "v2": loginButton]))
         
         loginButton.delegate = self
     }
